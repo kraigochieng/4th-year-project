a5bbd47035cdab6a69597381c8892f2e51857526
kraigochieng <kraigochieng@gmail.com>
2025-04-01 21:32:16 +0300

    added navbar

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -25,56 +25,55 @@
 		</div>
 	</SidebarProvider> -->
 
-	<NavigationMenu>
-		<NavigationMenuList>
-			<NavigationMenuItem>
-				<NavigationMenuLink href="/">Home</NavigationMenuLink>
-			</NavigationMenuItem>
-			<NavigationMenuItem>
-				<NavigationMenuTrigger>ADR</NavigationMenuTrigger>
-				<NavigationMenuContent>
-					<ul
-						class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]"
-					>
-						<li>
-							<NavigationMenuLink
-								as-child
-								class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-							>
-								<NuxtLink to="/adr/add">Add ADR</NuxtLink>
-							</NavigationMenuLink>
-						</li>
-						<li>
-							<NavigationMenuLink
-								as-child
-								class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
-							>
-								<NuxtLink to="/adr">View ADRs</NuxtLink>
-							</NavigationMenuLink>
-						</li>
-					</ul>
-				</NavigationMenuContent>
-			</NavigationMenuItem>
-			<NavigationMenuItem>
-				<Button @click="useAuthStore().logout()">Logout</Button>
-			</NavigationMenuItem>
-		</NavigationMenuList>
-	</NavigationMenu>
+	<div
+		class="flex items-center justify-between sticky top-0 z-50 bg-white border-b"
+	>
+		<NuxtLink href="/" class="flex items-center"
+			><Icon
+				name="material-symbols-light:settings-heart-outline-rounded"
+				size="64px"
+			/>
+			<p>MediLinda</p></NuxtLink
+		>
+
+		<NavigationMenu>
+			<NavigationMenuList>
+				<NavigationMenuItem>
+					<NavigationMenuLink href="/">Home</NavigationMenuLink>
+				</NavigationMenuItem>
+				<NavigationMenuItem>
+					<NavigationMenuTrigger>ADR</NavigationMenuTrigger>
+					<NavigationMenuContent>
+						<ul
+							class="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]"
+						>
+							<li>
+								<NavigationMenuLink
+									as-child
+									class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+								>
+									<NuxtLink to="/adr/add">Add ADR</NuxtLink>
+								</NavigationMenuLink>
+							</li>
+							<li>
+								<NavigationMenuLink
+									as-child
+									class="block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
+								>
+									<NuxtLink to="/adr">View ADRs</NuxtLink>
+								</NavigationMenuLink>
+							</li>
+						</ul>
+					</NavigationMenuContent>
+				</NavigationMenuItem>
+				<NavigationMenuItem>
+					<Button @click="useAuthStore().logout()">Logout</Button>
+				</NavigationMenuItem>
+			</NavigationMenuList>
+		</NavigationMenu>
+		<div>Avatar Here</div>
+	</div>
 
 	<slot></slot>
 </template>
-<script setup lang="ts">
-import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
-import NavigationMenu from "~/components/ui/navigation-menu/NavigationMenu.vue";
-import NavigationMenuItem from "~/components/ui/navigation-menu/NavigationMenuItem.vue";
-import NavigationMenuLink from "~/components/ui/navigation-menu/NavigationMenuLink.vue";
-import NavigationMenuList from "~/components/ui/navigation-menu/NavigationMenuList.vue";
-import NavigationMenuTrigger from "~/components/ui/navigation-menu/NavigationMenuTrigger.vue";
-import NavigationMenuContent from "~/components/ui/navigation-menu/NavigationMenuContent.vue";
-import Sidebar from "~/components/ui/sidebar/Sidebar.vue";
-import SidebarContent from "~/components/ui/sidebar/SidebarContent.vue";
-import SidebarMenuButton from "~/components/ui/sidebar/SidebarMenuButton.vue";
-import SidebarMenuItem from "~/components/ui/sidebar/SidebarMenuItem.vue";
-import SidebarProvider from "~/components/ui/sidebar/SidebarProvider.vue";
-import SidebarTrigger from "~/components/ui/sidebar/SidebarTrigger.vue";
-</script>
+<script setup lang="ts"></script>
