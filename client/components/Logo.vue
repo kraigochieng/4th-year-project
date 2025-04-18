30cb639caf99cebd966d0fa813298b7fa055ebae
kraigochieng <kraigochieng@gmail.com>
2025-04-18 06:21:04 +0300

    added logo as its own component

---
+++ client/components/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="flex items-center text-primary">
+		<Icon
+			name="material-symbols-light:settings-heart-outline-rounded"
+			size="64px"
+		/>
+		<p class="text-xl">
+			<span class="big-letter">M</span>edi<span class="big-letter">L</span
+			>inda
+		</p>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="css" scoped>
+/* * {
+	font-family: "Lexend";
+} */
+
+.big-letter {
+	@apply font-bold text-2xl;
+}
+</style>
